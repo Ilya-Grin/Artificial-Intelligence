4daf5219bd5ae752c072510958b05cbee6f6c288
Ilya-Grin <mrpsychopath2110@gmail.com>
2021-06-01 13:35:24 +0300

    main

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.header__navigation');
+const body = document.querySelector('body');
 
 function burgerClick() {
 	burger.classList.toggle('active');
 	menu.classList.toggle('active');
+	body.classList.toggle('overflow');
 }
