dc37f6794e136d1d012d0fb8d4669dcd5cbe8f43
Ilya-Grin <mrpsychopath2110@gmail.com>
2021-06-03 12:12:08 +0300

    main

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,39 @@ $(document).ready(function () {
 	});
 
 	$('.slider').slick({
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 10000,
 		infinite: true,
 		arrows: false,
-		dots: true,
+		speed: 300,
+		adaptiveHeight: true,
 		slidesToShow: 5,
-		slidesToScroll: 3
+		slidesToScroll: 2,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
 	});
 });
